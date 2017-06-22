efed3ca5b10576d978776d8405008f71dd23d3f1
Henrique Emidio <henriquehemidio@gmail.com>
2017-06-22 00:23:38 -0300

    front quase pronto

---
+++ NoBank/www/js/funcoes.js
@@ -0,0 +1,73 @@
+//Gera um array de números randômicos
+function gerarRandom() {
+  var arr = []
+  while(arr.length < 7){
+      var randomnumber = Math.ceil(Math.random()*100)
+      if(arr.indexOf(randomnumber) > -1) continue;
+      arr[arr.length] = randomnumber;
+  }
+  return arr;
+}
+
+//Contrói e renderiza o gráfico
+function renderChart(dados, elmID) {
+  var ctx = document.getElementById(elmID).getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'line',
+      data: {
+        labels: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+        datasets: [{
+          data: dados,
+          backgroundColor: "transparent",
+          borderColor:'#1BC191',
+        }]
+      },
+      options: {
+        title: {
+          display: false
+        },
+        legend: {
+                display: false
+        },
+        scales: {
+          xAxes: [{
+            display: false,
+              
+          }],
+          yAxes: [{
+              display: false,
+          }]
+      }
+    }
+  });
+  return myChart
+}
+
+//Exibe número de forma animada
+function animateNumbers(numero, elemento){
+//Animação da contagem de números
+var decimal_places = 2;
+var decimal_factor = decimal_places === 0 ? 1 : Math.pow(10, decimal_places);
+elemento
+  .animateNumber(
+    {
+      number: numero * decimal_factor,
+
+      numberStep: function(now, tween) {
+        var floored_number = Math.floor(now) / decimal_factor,
+            target = $(tween.elem);
+
+        if (decimal_places > 0) {
+          // force decimal places even if they are 0
+          floored_number = floored_number.toFixed(decimal_places);
+
+          // replace '.' separator with ','
+          floored_number = floored_number.toString().replace('.', ',');
+        }
+
+        target.text(floored_number);
+      }
+    },
+    600
+  );
+}
--- NoBank/www/js/index.js
+++ NoBank/www/js/index.js
@@ -37,6 +37,109 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        
+        myApp.onPageInit('index', function (page) {
+            
+            var myChart = renderChart([12, 34, 54, 11, 13, 6, 10], 'myChart')
+
+            $('.periodo').on('click', function(){
+              $('.periodo').removeClass('periodo-selecionado');
+              $(this).addClass('periodo-selecionado');
+              myChart.destroy()
+              renderChart(gerarRandom(), 'myChart')
+              animateNumbers(43852.57, $('.current-money'));
+            })
+
+            //Evento de abertura da página de detalhes da ação
+            $$('.stock-row').on('click', function(){
+              mainView.router.loadPage('ativo.html');
+            })
+
+            animateNumbers(43852.57, $('.current-money'));
+
+            //Eventos de click na tabbar
+            $('.tab-link').on('click', function(event){
+              return false;
+            });
+
+            $('#tab-carteira').on('click', function(){
+              if($(this).hasClass("active")){
+                return;
+              }
+              $('#current-money').html('00,00')
+              $('.tab-link').removeClass('active')
+              $(this).addClass('active')
+              changeTabEffect('#carteira')
+              $('.navbar-titulo').html('');
+              setTimeout(function(){ 
+                animateNumbers(43852.57, $('.current-money'))
+                myChart = renderChart([12, 34, 54, 11, 13, 6, 10], 'myChart')
+              }, 400);
+              
+            })
+
+            $('#tab-cotacoes').on('click', function(event){
+              if($(this).hasClass("active")){
+                return;
+              }
+              $('.tab-link').removeClass('active')
+              $(this).addClass('active')
+              changeTabEffect('#cotacoes')
+              $('.navbar-titulo').html('$4203.56');
+              $('.navbar-titulo').css('left', '0px');
+            });
+
+            $('#tab-transacoes').on('click', function(event){
+              if($(this).hasClass("active")){
+                return;
+              }
+              $('.tab-link').removeClass('active')
+              $(this).addClass('active')
+              changeTabEffect('#transacoes')
+              $$('.navbar-titulo').html('$4203.56');
+              $('.navbar-titulo').css('left', '0px');
+            });
+
+            $$('#tab-noticias').on('click', function(){
+              if($(this).hasClass("active")){
+                return;
+              }
+                $('.tab-link').removeClass('active')
+              $(this).addClass('active')
+              changeTabEffect('#noticias')
+              $('.navbar-titulo').html('$4203.56');
+              $('.navbar-titulo').css('left', '0px');
+            })
+
+            //Tabelas montante e preços
+            $$('#ativa-tab-valores').on('click', function(){
+              $(this).toggleClass('active');
+              $('#ativa-tab-montante').toggleClass('active');
+              $('#tabela-valores').toggleClass('none');
+              $('#tabela-montante').toggleClass('none');
+            })
+            $$('#ativa-tab-montante').on('click', function(){
+              $(this).toggleClass('active');
+              $('#ativa-tab-valores').toggleClass('active');
+              $('#tabela-valores').toggleClass('none');
+              $('#tabela-montante').toggleClass('none');
+            })
+
+            function changeTabEffect(show) {
+              function callb() {
+                $('div.active').removeClass();
+                $$('#icon-right').text('refresh');
+                $(show).slideToggle('fast');
+                $(show).addClass('active');
+                myChart.destroy()
+              }
+              $('div.active').animate(
+                {
+                  width: 'toggle'
+                }, 
+                200, 
+                callb
+              );
+            }
+        }).trigger();
     }
 };
