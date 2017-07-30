1b037ff5e65055724f5dc51b7b0f31e4e85f9280
Usuario <usuario@iMac-de-Usuario.local>
2017-07-30 20:55:47 -0300

    testando cor da status 6

--- NoBank/www/js/index.js
+++ NoBank/www/js/index.js
@@ -26,21 +26,20 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, true);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-      //alert('device-ready')
+      alert('device-ready')
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         alert("bhhj")
         
-        StatusBar.overlaysWebView(false);
         StatusBar.styleLightContent();
         StatusBar.backgroundColorByHexString("#009EC2");
         
